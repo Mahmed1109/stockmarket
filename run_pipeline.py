0b8016f3b23228655b4557a2293b8286aaa4814c
Mahmed1109 <m.ahmed110900@gmail.com>
2025-07-28 10:12:39 +0100

    date

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,20 +1,25 @@
 import pandas as pd
+from datetime import datetime, timedelta
 from src.fetch_stock import get_stock_data
 from src.fetch_sentiment import attach_sentiment
 from src.feature_engineer import add_features
 from src.train_model import train_model
 from src.evaluate_model import evaluate
 
+
 def main():
     ticker = "AAPL"
-    start_date = "2020-01-01"
-    end_date = "2025-01-01"
 
-    
+    # newsapi only allows 30days, gotta pay for more
+    end_date = datetime.today().date()
+    start_date = end_date - timedelta(days=30)
+
+    start_date = start_date.strftime("%Y-%m-%d")
+    end_date = end_date.strftime("%Y-%m-%d")
+
     df = get_stock_data(ticker, start_date, end_date)
     df.index = pd.to_datetime(df.index)
 
-    
     df = attach_sentiment(df, ticker)
 
     df = add_features(df)
